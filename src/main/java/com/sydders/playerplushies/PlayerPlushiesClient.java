54a9ced602aa484ec7f59b68623ffa0b45f7e2ff
SydneyEwenss <sydneyewens06@gmail.com>
2025-03-29 05:50:38 +0000

    Initial commit

---
+++ src/main/java/com/sydders/playerplushies/PlayerPlushiesClient.java
@@ -0,0 +1,11 @@
+package com.sydders.playerplushies;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlayerPlushiesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
